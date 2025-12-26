8e77a11a078c216c8e78e0eec00e18a38d479730
Wendel Miranda <wendelllllmiranda@gmail.com>
2025-12-25 22:24:43 -0300

    Adiciona tratamento de erros para entrada de números inteiros

--- projeto.py
+++ projeto.py
@@ -1,22 +1,26 @@
-# Lê um número inteiro digitado pelo usuário
-numero = int(input("Digite um número inteiro: "))
+try:
+    # Lê um número inteiro digitado pelo usuário
+    numero = int(input("Digite um número inteiro: "))
 
-# Verifica se o número é negativo e ímpar
-if numero < 0 and numero % 2 != 0:
-    print("o número é negativo e ímpar")
+    # Verifica se o número é negativo e ímpar
+    if numero < 0 and numero % 2 != 0:
+        print("O número é negativo e ímpar")
 
-# Verifica se o número é negativo e par
-elif numero < 0 and numero % 2 == 0:
-    print("o número é negativo e par")
+    # Verifica se o número é negativo e par
+    elif numero < 0 and numero % 2 == 0:
+        print("O número é negativo e par")
 
-# Verifica se o número é positivo e ímpar
-elif numero > 0 and numero % 2 != 0:
-    print("o número é positivo e ímpar")
+    # Verifica se o número é positivo e ímpar
+    elif numero > 0 and numero % 2 != 0:
+        print("O número é positivo e ímpar")
 
-# Verifica se o número é positivo e par
-elif numero > 0 and numero % 2 == 0:
-    print("o número é positivo e par")
+    # Verifica se o número é positivo e par
+    elif numero > 0 and numero % 2 == 0:
+        print("O número é positivo e par")
 
-# Caso o número seja zero
-else:
-    print("o número é zero")
+    # Caso o número seja zero
+    else:
+        print("O número é zero")
+
+except ValueError:
+    print("Erro: digite apenas número inteiro!")
